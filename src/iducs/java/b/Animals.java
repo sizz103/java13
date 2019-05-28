5c3159ae2904cb513f1c6ed172fe2f9d5d428562
sizz103 <sizz103@nate.com>
2019-05-28 17:38:20 +0900

    master animals

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -2,7 +2,7 @@ package iducs.java.b;
 
 public interface Animals {
 	String species = "동물"; // public final 생략
-	
+	String common = "공통점"; // public final 생략
 	void move(); //public abstract생략
 	void eat();
 	void sound();
